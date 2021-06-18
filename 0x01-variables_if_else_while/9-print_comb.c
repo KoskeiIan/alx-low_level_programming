2f098555cf61c993041eb7b768178d21972f6a00
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-18 23:37:09 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-  int n;
+int n;
 
-  for (n = 0; n < 10; num++)
-    {
-      putchar(n + '0');
-      if (n != 9)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
