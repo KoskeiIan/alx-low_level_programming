8cdbe18e9c0c0ad8b5545670cd3c79835d4bf8df
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 01:23:46 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- *_islower - return 1 if c is lowercase, 0 if it is not 
+ *_islower - return 1 if c is lowercase, 0 if it is not
  *
  *Retunrn: 1 or 0
  *
  */
 int _islower(int c)
 {
-  return (c >= 97 && c <= 122);
+return (c >= 97 && c <= 122);
 }
