11600bff1c8b56d7c216ed8540aab9d02d252ce9
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-22 23:28:18 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
  *print_last_digit - prints the last digit 
- *
+ *@n: integer to be tested
  *Return: value of the last digit 
  */
 int print_last_digit(int n)
 {
-  if ((n % 10) >= 0)
-    {
-    _putchar ('0' + (n % 10));
-  return (n % 10);
+if ((n % 10) >= 0)
+{
+_putchar ('0' + (n % 10));
+return (n % 10);
 }
- else
-   {
-    _putchar('0' + (-1 * (n % 10)));
-		return ((-1 * (n % 10)));
- }
- }
+else
+{
+_putchar('0' + (-1 * (n % 10)));
+return ((-1 * (n % 10)));
+}
+}
