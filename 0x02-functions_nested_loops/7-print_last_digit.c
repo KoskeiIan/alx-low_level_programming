2932a0b3bc33ec0178c73baea8bfc7b4f5ae451e
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-22 23:23:25 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@ int_print_last_digit(int n)
 {
   if ((n % 10) >= 0)
     {
-    _putchar ('0' + (nn % 10));
+    _putchar ('0' + (n % 10));
   return (n % 10);
 }
  else
    {
-     _putchar('0' + (n % 10));
-     return ((-1 * (n % 10)));
+    _putchar('0' + (-1 * (n % 10)));
+		return ((-1 * (n % 10)));
  }
  }
