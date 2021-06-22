fab5c7f2ab43a4a179883252a6538b2f93e27198
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-22 07:17:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - Print alphabet
+ *
+ *Return: (void)
+ *
+ */
+void print_alphabet_x10(void)
+{
+char data = 'a';
+int counter = 1;
+ehile (counter <= 10)
+{
+while (data <= 'z')
+{
+putchar(data);
+data++;
+}
+data = 'a';
+}
+data = 'a';
+putchar('\n');
+counter++;
+}
