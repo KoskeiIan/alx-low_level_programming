dd91684ec233ea2b67a3e45754ab1e266421f42d
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-22 23:15:07 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,26 +7,30 @@
  *
  */
 
-int print_sign(int n)
+// int print_sign(int n)
+// {
+// if (n > 0)
+// {
+// _putchar('+');
+// return (1);
+//  }
+//  else (n < 0)
+//    {
+//      _putchar('+');
+//      return (1);
+//    }
+//      else if (n < 0);
+// {
+//   _putchar('-');
+//   return (-1);
+//  }
+//  else
+//    {
+//      _putchar('0');
+//      return (0);
+//    }
+// }
+int _abs(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
- }
- else (n < 0)
-   {
-     _putchar('+');
-     return (1);
-   }
-     else if (n < 0);
-{
-  _putchar('-');
-  return (-1);
- }
- else
-   {
-     _putchar('0');
-     return (0);
-   }
+	return (n < 0 ? -1 * n : n);
 }
