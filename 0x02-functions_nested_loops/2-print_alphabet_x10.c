9addb679817c2ee3d4639e8247087a06ff3b5612
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 01:17:57 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,12 +13,10 @@ char c;
 
 for (i = 0; i < 10; i++)
 {
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n');
- }
-return (0);
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
 }
-
+_putchar('\n');
+}
+}
