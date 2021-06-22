a461f4cbdcb47f8b382ea93e4c406925f90ec10f
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 02:04:34 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,23 @@
  */
 void times_table(void)
 {
-  int x,y;
+int x,y;
 
-  for (y = 0; y < 10; x++)
-    {
-      for (x = 0; x < 10; x++)
-	{
-	  if (((x * y) / 10) > 0)
-	    _putchar('o' + ((x * y) / 10));
-	  else if (x > 0)
-	    _putchar(' ');
-	  _putchar('0' + ((x * y) % 10));
-	  if (x < 9)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	}
-      _putchar('\n');
-    }
+for (y = 0; y < 10; x++)
+{
+for (x = 0; x < 10; x++)
+{
+if (((x * y) / 10) > 0)
+_putchar('0' + ((x * y) / 10));
+else if (x > 0)
+_putchar(' ');
+_putchar('0' + ((x * y) % 10));
+if (x < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
