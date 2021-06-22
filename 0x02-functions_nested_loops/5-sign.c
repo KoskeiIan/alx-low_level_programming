7d7a1f315616dd0c3c5fa0db415a040b9321a22d
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 01:36:22 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,15 @@
  *
  */
 
+#include "holberton.h"
+
+/**
+ *print_sign - check if n is positive, negative or zero
+ *
+ *Return: 1 if n is positive, -1 if negative and 0 otherwise
+ *
+ */
+
 int print_sign(int n)
 {
   if (n > 0)
@@ -14,14 +23,14 @@ int print_sign(int n)
       _putchar('+');
       return (1);
     }
-  else if (n = 0)
+  else if (n > 0)
     {
-      _putchar('0');
-      return (0);
+      _putchar('-');
+      return (-1);
     }
   else
     {
-      _putchar('-');
-	return (-1);
+      _putchar('0');
+	return (0);
     }
 }
