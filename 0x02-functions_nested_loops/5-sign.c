b5203825f423e4f40da6e34a74ac4f56e0b11cb2
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 01:33:27 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  *print_sign - check if n is positive, negative or zero
- *
+ *@n: interger to be tested
  *Return: 1 if n is positive, -1 if negative and 0 otherwise
  *
  */
@@ -14,14 +14,14 @@ int print_sign(int n)
       _putchar('+');
       return (1);
     }
-  else if (n > 0)
+  else if (n = 0)
     {
-      _putchar('-');
-      return (-1);
+      _putchar('0');
+      return (0);
     }
   else
     {
-      _putchar('0');
-	return (0);
+      _putchar('-');
+	return (-1);
     }
 }
