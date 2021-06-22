eb783c37ef4c897b8672af98b7dcd5ba4051476b
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-23 02:51:56 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10(void) - prints the alphabet in lowercase ten times
+ * print_alphabet_x10 - prints the alphabet in lowercase ten times
  *
  *
  */
