f4cec9065f1066f1675b2ca545cb15dde66da0fe
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-22 22:28:56 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -38,3 +38,75 @@ inrt powB(int base, int power)
 	length += 1;
       }
     return
+
+/**
+ * putnchar - print a number of any digit
+ * @num: takes an input number
+ *
+ *
+ */
+void putnchar(int num)
+{
+	int length = numLength(num), j = length - 1, k, tmp2, digit1;
+
+	if (num == 0)
+		_putchar(48);
+	else
+	{
+		while (j >= 0)
+		{
+			if (num % powB(10, j) == 0 && j != 0)
+			{
+				_putchar(48 + num / powB(10, j));
+				for (k = j; k > 0; k--)
+				{
+					_putchar(48);
+				}
+				j = -1;
+			}
+			else
+			{
+				digit1 = num / powB(10, j);
+				_putchar(digit1 + 48);
+				tmp2 = num;
+				num -= powB(10, j) * digit1;
+				if (numLength(tmp2) - numLength(num) == 2)
+				{
+					_putchar(48);
+					j--;
+				}
+				j--;
+			}
+		}
+	}
+}
+
+/**
+ * sumr - return the sum of numbers from start to end with steps steps
+ * @start: the first number
+ * @end: the last number
+ * @step: the number of steps
+ * Return: sum
+ */
+int sumr(int start, int end, int step)
+{
+	int sum = 0, i;
+
+	for (i = start; i < end; i += step)
+	{
+		sum += i;
+	}
+	return (sum);
+}
+
+/**
+ * main - prints the sum of all numbers under 1024
+ * that are divisible by 3 or 5
+ * Return: 0
+ */
+int main(void)
+{
+	putnchar(sumr(3, 1024, 3) + sumr(5, 1024, 5) - sumr(15, 1024, 15));
+	_putchar('\n');
+	return (0);
+}
