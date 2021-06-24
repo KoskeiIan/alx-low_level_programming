1f7e7fb79fcbe6f42079b2f1b168605942868bab
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-24 11:35:10 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,18 @@
 
 /**
  *_isupper - checks if the letter is uppercase
- *@c: integer to be checked 
+ *@c: integer to be checked
  *Return: 1 if c is uppercase
  */
 
 int _isupper(int c)
- {
-   if ((c >= 65) && (c <= 90))
-     {
-	   return (1);
-	 }
-       else
-	 {
-	   return (0);
-	 }
-     }
+{
+if ((c >= 65) && (c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
