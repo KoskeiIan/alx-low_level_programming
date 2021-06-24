4ba52029c5a030ba7f6e719a197b7e9ad56d652a
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-24 05:30:44 -0700

    0x04 test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#invlude "holberton.h"
+
+/**
+ *_isupper - checks if the letter is uppercase
+ *@c: integer to be checked 
+ *Return: 1 if c is uppercase
+ */int _isupper(int c)
+ {
+   if ((c >= 'A') && (c <= 'z'))
+     {
+       {
+	   return (1);
+	 }
+       else
+	 {
+	   return (0);
+	 }
+     }
