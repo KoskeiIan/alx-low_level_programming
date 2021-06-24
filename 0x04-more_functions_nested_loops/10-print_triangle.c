9276b65d74bf9a15b05bf98897efc86bdeaaf49f
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-24 12:40:11 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,26 +8,26 @@
 
 void print_triangle(int size)
 {
-  int i = 1, ii;
+int i = 1, ii;
 
-  while (i <= size && size > 0)
-    {
-      ii = 0;
-      while (ii < size - 1)
-	{
-	  _putchar(' ');
-	  ii++;
-	}
-      ii = 0;
-      while (ii < i)
-	{
-	  _putchar('#');
-	  ii++;
-	}
+while (i <= size && size > 0)
+{
+ii = 0;
+while (ii < size - 1)
+{
+_putchar(' ');
+ii++;
+}
+ii = 0;
+while (ii < i)
+{
+_putchar('#');
+ii++;
+}
 
-      _putchar('\n');
-      i++;
-    }
-  if (i == 1)
-    _putchar('\n');
+_putchar('\n');
+i++;
+}
+if (i == 1)
+_putchar('\n');
 }
