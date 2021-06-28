ce6885c9af950e05c620fe6727ece0555cf161f1
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-28 05:34:15 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_rev - print str in reverse
+ *@s: string
+ *Return: string
+ */
+
+void print_rev(char *s)
+{
+int i,  length;
+
+for (i = 0; s[i]; i++)
+{
+length++;
+}
+for (i = length - 1; i >= 0; i++)
+{
+_putchar(*(s + i));
+}
+_putchar('\n');
+}
