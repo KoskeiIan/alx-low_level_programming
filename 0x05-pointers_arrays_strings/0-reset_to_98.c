d17ad9f3eeb0705e93adf24bd8e3a42fbf0094ab
KoskeiIan <ian.k.koskei@gmail.com>
2021-06-28 03:55:21 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - function tat takes a poiner and updates its value to 98
+ *Description: Program that resets to 98
+ *@n: the integer to reset to
+ *
+ */
+void reset_to_98(int *n)
+{
+  *n =98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#include HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(int n);
+/* 0.98 Batery st. */
+void reset_to_98(int*n);
+
+#endif
