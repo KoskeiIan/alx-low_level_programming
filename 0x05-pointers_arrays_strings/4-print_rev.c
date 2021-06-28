efc0a2e630b804d39ae9af4686b2495168a704f4
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-28 23:46:56 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,18 @@
 
 void print_rev(char *s)
 {
-int i,  length;
+int length, last;
 
-for (i = 0; s[i]; i++)
-{
-length++;
-}
-for (i = length - 1; i >= 0; i--)
-{
-_putchar(*(s + i));
-}
-_putchar('\n');
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	last = length - 1;
+	for (; last >= 0; last--)
+	{
+		_putchar(s[last]);
+	}
+	_putchar('\n');
 }
