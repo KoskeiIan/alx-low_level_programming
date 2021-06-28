54b13f56c3932bc877365930c13ec1162c3aa0a7
KoskeiIan <78481742+KoskeiIan@users.noreply.github.com>
2021-06-28 23:39:50 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,24 +9,16 @@ REturn: 0
 */
 int main(void)
 {
-  srand(time(NULL))
-    int i, j, escii = 1772, sum = 0, r;
-  int pass[1000];
-  for(i = 0; sum < 1772; i++)
-    {r = ran d () % ( (127 + 1 -33)) = 33;
-      sum += r;
-      if (sum > escii && sum - escii <= 33){
-	if(*(pass = i -2) += sum - escii;
-	   else
-	     *(pass = i) = r;
-	   }
-	sum = 0;
-	for (j = 0; j < i; j++)
-	  {
-	    printf(;s[%d], ', pass[j]);
-	    sum += pass[j];
+  int sum;
+	char c;
 
-	  }
-	printf('sum %d',sum );
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 	return (0);
-      }
+}
