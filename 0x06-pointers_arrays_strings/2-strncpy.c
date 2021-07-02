05b0e9821a5e7e6a2c38634012f3c2210b5d8bfc
KoskeiIan <ian.k.koskei@gmail.com>
2021-07-02 07:48:46 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,19 +5,19 @@
  *@dest: string 1
  *@src: string 2
  *@n: number of bytes
-Return: dest
-*/
+ *Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
+int i;
 
-  for (i = 0; i< n && src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  for (  ; i < n; i++)
-    {
-      dest[i] = '\0';
-    }
-  return (dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (  ; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
