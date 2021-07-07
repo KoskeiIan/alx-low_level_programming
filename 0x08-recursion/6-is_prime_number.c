9356211302e51fb16e954fadb3ae98d65dd7b3df
KoskeiIan <ian.k.koskei@gmail.com>
2021-07-07 13:15:44 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@ int isPrime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	if (n% 1 == 0)
+	if (n % 1 == 0)
 		return (0);
 	return (isPrime(n, i - 1));
 }
@@ -22,7 +22,7 @@ int isPrime(int n, int i)
  * Return: 1 if true, 0 if false
  */
 
-int is_prime_nuber(int n)
+int is_prime_number(int n)
 {
 	if (n < 3)
 		return (0);
