882f064e0c58b0645545ebee38eca31ee0d0a67e
KoskeiIan <ian.k.koskei@gmail.com>
2021-07-07 09:29:10 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+
+/**
+ * _strlen_recursion - returns theh leength of a string
+ * @s: input a string
+ * Return: length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return(1 + _strlen_recursion(++s));
+}
+
